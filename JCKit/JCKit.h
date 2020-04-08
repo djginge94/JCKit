bbbacd2f460a800de8a89fdc600c028a0329b6c7
Joshua Colley <joshua@Joshuas-iMac.local>
2020-04-08 23:34:16 +0100

    Initial project setup

---
+++ JCKit/JCKit.h
@@ -0,0 +1,19 @@
+//
+//  JCKit.h
+//  JCKit
+//
+//  Created by Joshua Colley on 08/04/2020.
+//  Copyright © 2020 Joshua Colley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JCKit.
+FOUNDATION_EXPORT double JCKitVersionNumber;
+
+//! Project version string for JCKit.
+FOUNDATION_EXPORT const unsigned char JCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JCKit/PublicHeader.h>
+
+
---
+++ JCKitExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JCKitExamples
+//
+//  Created by Joshua Colley on 08/04/2020.
+//  Copyright © 2020 Joshua Colley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
